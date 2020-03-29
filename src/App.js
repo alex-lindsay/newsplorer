8c2644c1ccd4674a26e5c2c4bc4a4d0ecf4b790a
Alex Lindsay <work@alex-lindsay.com>
2020-03-29 14:05:27 -0700

    Install material-ui and Roboto font

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App" data-testid="app"></div>;
+  return (
+    <div className="App" data-testid="app">
+      <h1>This is a test.</h1>
+    </div>
+  );
 }
 
 export default App;
