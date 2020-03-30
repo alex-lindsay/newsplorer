e9e03ce8345107ed8f80f7b9fb47836ca69f36ce
Alex Lindsay <work@alex-lindsay.com>
2020-03-30 10:23:51 -0700

    Add missing action for countries

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,6 +3,7 @@ export const GET_HEADLINES = "GET_HEADLINES";
 export const GET_SOURCES = "GET_SOURCES";
 export const GET_CATEGORIES = "GET_CATEGORIES";
 export const GET_LANGUAGES = "GET_LANGUAGES";
+export const GET_COUNTRIES = "GET_COUNTRIES";
 
 export const GET_STORY = "GET_STORY";
 
@@ -28,6 +29,12 @@ export function getCategories() {
 export function getLanguages() {
   return {
     type: GET_LANGUAGES,
+  };
+}
+
+export function getCountries() {
+  return {
+    type: GET_COUNTRIES,
   };
 }
 
