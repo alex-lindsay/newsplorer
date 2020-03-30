2de3776b1dfaffda02a7e3a194ca91956b75fa61
Alex Lindsay <work@alex-lindsay.com>
2020-03-30 10:23:35 -0700

    Import sample data for initial state

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,8 +1,19 @@
+import { sampleHeadlines } from "../../data/sample_headlines";
+import { sampleSources } from "../../data/sample_sources";
+import { sampleCategories } from "../../data/sample_categories";
+import { sampleLanguages } from "../../data/sample_languages";
+import { sampleCountries } from "../../data/sample_countries";
+
 const initialState = {
   showStory: false,
-  sources: [],
-  categories: [],
-  languages: [],
-  headlines: [],
-  story: [],
+  sources: sampleSources.sources,
+  categories: sampleCategories,
+  languages: sampleLanguages,
+  country: sampleCountries,
+  headlines: sampleHeadlines.articles,
+  source: null,
+  category: null,
+  language: null,
+  country: null,
+  story: null,
 };
