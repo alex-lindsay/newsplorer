a272cb78b799ba6b5021b0761d928cf11619e3ca
Alex Lindsay <work@alex-lindsay.com>
2020-04-01 12:41:39 -0700

    Clean up actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,52 +1,14 @@
 /* Action Types */
-export const GET_HEADLINES = "GET_HEADLINES";
-export const GET_SOURCES = "GET_SOURCES";
-export const SET_SOURCE = "SET_SOURCE";
-export const GET_CATEGORIES = "GET_CATEGORIES";
-export const SET_CATEGORY = "SET_CATEGORY";
-export const GET_LANGUAGES = "GET_LANGUAGES";
-export const SET_LANGUAGE = "SET_LANGUAGE";
-export const GET_COUNTRIES = "GET_COUNTRIES";
 export const SET_COUNTRY = "SET_COUNTRY";
-
-export const GET_STORY = "GET_STORY";
+export const SET_LANGUAGE = "SET_LANGUAGE";
+export const SET_CATEGORY = "SET_CATEGORY";
+export const SET_SOURCE = "SET_SOURCE";
 
 /* Action Creators */
-export function getHeadlines() {
+export function setCountry(country) {
   return {
-    type: GET_HEADLINES,
-  };
-}
-
-export function getSources() {
-  return {
-    type: GET_SOURCES,
-  };
-}
-
-export function setSource(source) {
-  return {
-    type: SET_SOURCE,
-    source: source,
-  };
-}
-
-export function getCategories() {
-  return {
-    type: GET_CATEGORIES,
-  };
-}
-
-export function setCategory(category) {
-  return {
-    type: SET_CATEGORY,
-    category: category,
-  };
-}
-
-export function getLanguages() {
-  return {
-    type: GET_LANGUAGES,
+    type: SET_COUNTRY,
+    country: country,
   };
 }
 
@@ -57,21 +19,16 @@ export function setLanguage(language) {
   };
 }
 
-export function getCountries() {
+export function setCategory(category) {
   return {
-    type: GET_COUNTRIES,
+    type: SET_CATEGORY,
+    category: category,
   };
 }
 
-export function setCountry(country) {
+export function setSource(source) {
   return {
-    type: SET_COUNTRY,
-    country: country,
-  };
-}
-
-export function getStory() {
-  return {
-    type: GET_STORY,
+    type: SET_SOURCE,
+    source: source,
   };
 }
