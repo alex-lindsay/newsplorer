8fe98cb1791cd975670fe93e8d350213d52d1882
Alex Lindsay <work@alex-lindsay.com>
2020-04-02 21:54:02 -0700

    Add image; format date

--- src/containers/Headlines/Headlines.js
+++ src/containers/Headlines/Headlines.js
@@ -1,24 +1,41 @@
 import React, { Component } from "react";
+import { parseISO, format } from "date-fns";
 
 import styles from "./Headlines.module.css";
 
-import { Container, Paper } from "@material-ui/core";
+import Container from "@material-ui/core/Container";
 import TableContainer from "@material-ui/core/TableContainer";
 import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import Tooltip from "@material-ui/core/Tooltip";
+import Paper from "@material-ui/core/Paper";
 
 class Headlines extends Component {
   render() {
     let tableRows = this.props.headlines
       ? this.props.headlines.map((row, index) => (
           <TableRow key={index}>
-            <TableCell align="center"></TableCell>
-            <TableCell align="left">{row.title}</TableCell>
+            <TableCell align="center">
+              {row.urlToImage ? (
+                <Paper className={styles.HeadlineImage} elevation={3} square>
+                  <img src={row.urlToImage} width="100" alt={row.description} />
+                </Paper>
+              ) : null}
+            </TableCell>
+            <Tooltip
+              title={row.description}
+              placement="bottom"
+              aria-label={row.description}
+            >
+              <TableCell align="left">{row.title}</TableCell>
+            </Tooltip>
             <TableCell align="left">{row.author}</TableCell>
-            <TableCell align="center">{row.publishedAt}</TableCell>
+            <TableCell align="center">
+              {format(parseISO(row.publishedAt), "LLL d h:mmaaaaa")}
+            </TableCell>
           </TableRow>
         ))
       : null;
