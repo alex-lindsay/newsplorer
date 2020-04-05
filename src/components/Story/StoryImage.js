12a4160f478fc7f790019eb3127053a375094f81
Alex Lindsay <work@alex-lindsay.com>
2020-04-05 11:09:29 -0700

    Cleanup commit

--- src/components/Story/StoryImage.js
+++ src/components/Story/StoryImage.js
@@ -1,9 +1,7 @@
 import React from "react";
 import styles from "./Story.module.css";
 
-import Typography from "@material-ui/core/Typography";
-
-const storyImage = props => {
+const storyImage = (props) => {
   return (
     <div className={styles.StoryImage}>
       <img src={props.imageUrl} alt={props.description} />
