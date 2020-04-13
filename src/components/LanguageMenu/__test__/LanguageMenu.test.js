931a5672c81ef18702f41b04a806c2fb089f368b
Alex Lindsay <work@alex-lindsay.com>
2020-04-13 11:36:36 -0700

    Code coverage for LanguageMenu

--- src/components/LanguageMenu/__test__/LanguageMenu.test.js
+++ src/components/LanguageMenu/__test__/LanguageMenu.test.js
@@ -1,15 +1,77 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { shallow, mount } from "enzyme";
+
 import LanguageMenu from "../LanguageMenu";
 
-afterEach(cleanup);
+import { sampleLanguages } from "../../../data/sample_languages";
+import { ExpansionPanelActions, MenuItem } from "@material-ui/core";
 
-it("renders App without crashing", () => {
-  const { getByTestId } = render(<LanguageMenu />);
-  expect(getByTestId("language-menu")).toBeInTheDocument();
+const mockSetLanguage = jest.fn((event) => {
+  // console.log(event);
+  return event;
 });
 
-it("matches expected snapshot", () => {
-  const { asFragment } = render(<LanguageMenu />);
-  expect(asFragment()).toMatchSnapshot();
+describe("LanguageMenu", () => {
+  it("renders with no props", () => {
+    const wrapper = mount(<LanguageMenu />);
+  });
+
+  it("renders specifed languages", () => {
+    const wrapper = mount(<LanguageMenu languages={sampleLanguages} />);
+    expect(wrapper.exists("#menu-languagemenu"));
+    expect(wrapper.find("#menu-languagemenu li")).toHaveLength(
+      sampleLanguages.length + 1
+    );
+    expect(wrapper.find("#menu-languagemenu li").first().text()).toBe("None");
+    expect(wrapper.exists(".MuiBadge-badge"));
+    expect(wrapper.find(".MuiBadge-badge").text()).toBe("");
+  });
+
+  it("renders the selected language", () => {
+    const wrapper = mount(
+      <LanguageMenu languages={sampleLanguages} language={sampleLanguages[3]} />
+    );
+    expect(wrapper.exists("#menu-languagemenu"));
+    expect(wrapper.exists(".MuiBadge-badge"));
+    expect(wrapper.find(".MuiBadge-badge").text()).toBe(sampleLanguages[3]);
+  });
+
+  it("sets the selected language when list item is clicked", () => {
+    const wrapper = mount(
+      <LanguageMenu
+        languages={sampleLanguages}
+        language={null}
+        setLanguage={mockSetLanguage}
+      />
+    );
+    wrapper
+      .find("#menu-languagemenu")
+      .first()
+      .find("li")
+      .at(4)
+      .simulate("click");
+    expect(mockSetLanguage).toHaveBeenCalled();
+    // expects 3 because li[0] is the manually added "None"
+    expect(mockSetLanguage.mock.results[0].value).toBe(sampleLanguages[3]);
+  });
+
+  it("shows/hides menu when icon is clicked", () => {
+    const wrapper = mount(
+      <LanguageMenu
+        languages={sampleLanguages}
+        language={null}
+        setLanguage={mockSetLanguage}
+      />
+    );
+    const menu = wrapper.find("div#menu-languagemenu").first();
+    const menuAttributes = menu.getDOMNode().attributes;
+
+    expect(menuAttributes.getNamedItem("aria-hidden").value).toBe("true");
+
+    wrapper.find("button").first().simulate("click");
+    expect(menuAttributes.getNamedItem("aria-hidden")).toBeNull();
+
+    wrapper.find(MenuItem).first().simulate("click");
+    expect(menuAttributes.getNamedItem("aria-hidden").value).toBe("true");
+  });
 });
