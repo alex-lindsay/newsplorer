84b3b4bf0173e588ff21af3e741b82763454a419
Alex Lindsay <work@alex-lindsay.com>
2020-04-13 17:03:46 -0700

    Fix errors in SourceMenu test

--- src/components/SourceMenu/__test__/SourceMenu.test.js
+++ src/components/SourceMenu/__test__/SourceMenu.test.js
@@ -1,15 +1,77 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { shallow, mount } from "enzyme";
+
 import SourceMenu from "../SourceMenu";
 
-afterEach(cleanup);
+import { sampleSources } from "../../../data/sample_sources";
+import { ExpansionPanelActions, MenuItem } from "@material-ui/core";
 
-it("renders App without crashing", () => {
-  const { getByTestId } = render(<SourceMenu />);
-  expect(getByTestId("source-menu")).toBeInTheDocument();
+const mockSetSource = jest.fn((event) => {
+  // console.log(event);
+  return event;
 });
 
-it("matches expected snapshot", () => {
-  const { asFragment } = render(<SourceMenu />);
-  expect(asFragment()).toMatchSnapshot();
+describe("SourceMenu", () => {
+  // it("renders with no props", () => {
+  //   const wrapper = mount(<SourceMenu />);
+  // });
+
+  it("renders specifed sources", () => {
+    const wrapper = mount(<SourceMenu sources={sampleSources.sources} />);
+    expect(wrapper.exists("#menu-sourcemenu"));
+    expect(wrapper.find("#menu-sourcemenu li")).toHaveLength(
+      sampleSources.sources.length + 1
+    );
+    expect(wrapper.find("#menu-sourcemenu li").first().text()).toBe("None");
+    expect(wrapper.exists("h6"));
+    expect(wrapper.find("h6").text()).toBe("");
+  });
+
+  it("renders the selected source", () => {
+    const wrapper = mount(
+      <SourceMenu
+        sources={sampleSources.sources}
+        source={sampleSources.sources[3].id}
+      />
+    );
+    expect(wrapper.exists("#menu-sourcemenu"));
+    expect(wrapper.exists("h6"));
+    expect(wrapper.find("h6").text()).toBe(sampleSources.sources[3].id);
+  });
+
+  it("sets the selected source when list item is clicked", () => {
+    const wrapper = mount(
+      <SourceMenu
+        sources={sampleSources.sources}
+        source={null}
+        setSource={mockSetSource}
+      />
+    );
+    wrapper.find("#menu-sourcemenu").first().find("li").at(4).simulate("click");
+    expect(mockSetSource).toHaveBeenCalled();
+    // expects 3 because li[0] is the manually added "None"
+    expect(mockSetSource.mock.results[0].value).toBe(
+      sampleSources.sources[3].id
+    );
+  });
+
+  it("shows/hides menu when icon is clicked", () => {
+    const wrapper = mount(
+      <SourceMenu
+        sources={sampleSources.sources}
+        source={null}
+        setSource={mockSetSource}
+      />
+    );
+    const menu = wrapper.find("div#menu-sourcemenu").first();
+    const menuAttributes = menu.getDOMNode().attributes;
+
+    expect(menuAttributes.getNamedItem("aria-hidden").value).toBe("true");
+
+    wrapper.find("button").first().simulate("click");
+    expect(menuAttributes.getNamedItem("aria-hidden")).toBeNull();
+
+    wrapper.find(MenuItem).first().simulate("click");
+    expect(menuAttributes.getNamedItem("aria-hidden").value).toBe("true");
+  });
 });
